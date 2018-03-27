5f84314c5e889f5f67dbced908153eb65c66bbdc
DESKTOP-153JO0O\Raghav <raghavdonakanti@gmail.com>
2018-03-27 15:49:12 +0530

    able to log out the tank playercontroller is possesing

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -4,9 +4,29 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	//check to see if controller is posseng  a tank
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController is not possesing tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController is possessing %s"), *ControlledTank->GetName());
+	}
+	UE_LOG(LogTemp,Warning,TEXT(""))
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
-	return Cast<ATank>(GetPawn());
+	return Cast<ATank>(GetPawn());//return pointer to ATank
+	
 }
+
+
 
 
